7a327c8bad1bcc3ea87bd396d289f5d6745dab1f
sunfly <>
2021-01-08 20:49:26 +0800

    【修改】

--- lib/main/performance/widgets/delay_build_widget.dart
+++ lib/main/performance/widgets/delay_build_widget.dart
@@ -1,3 +1,4 @@
+import 'dart:collection';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -8,9 +9,17 @@ class GridInfo {
   final String url;
   final String title;
   final String icon;
+  final String aboveIcon;
   final String subTitle;
+  final int index;
 
-  GridInfo({@required this.icon, @required this.url, @required this.title, @required this.subTitle});
+  GridInfo(
+      {@required this.index,
+      @required this.icon,
+      @required this.url,
+      @required this.title,
+      @required this.subTitle,
+      @required this.aboveIcon});
 }
 
 class DelayBuildWidgetTestPage extends StatefulWidget {
@@ -18,8 +27,12 @@ class DelayBuildWidgetTestPage extends StatefulWidget {
   _DelayBuildWidgetTestPageState createState() => _DelayBuildWidgetTestPageState();
 }
 
-class _DelayBuildWidgetTestPageState extends State<DelayBuildWidgetTestPage> {
+class _DelayBuildWidgetTestPageState extends State<DelayBuildWidgetTestPage> with SingleTickerProviderStateMixin {
   List<GridInfo> dataList = [];
+  ScrollController _firstScrollController = ScrollController();
+  ScrollController _secondScrollController = ScrollController();
+  TabController _tabController;
+  PageController _pageController = PageController();
   List<String> imageUrls = [
     '2774198',
     '6194942',
@@ -73,17 +86,37 @@ class _DelayBuildWidgetTestPageState extends State<DelayBuildWidgetTestPage> {
     '4468058',
     '5914166',
     '6291566',
+    '4621378',
+    '5965972',
+    '5913391',
+    '5312333',
+    '5101217',
+    '5855527',
+    '6061293',
+    '5770047',
+    '4558306',
+    '2109762',
+    '5913949',
+    '6070129',
+    '2886574',
+    '6032603',
+    '5615782',
+    '5997712',
   ];
   @override
   void initState() {
     super.initState();
+    _tabController = TabController(length: 2, vsync: this);
     String content = '拉开就分开了拉萨附近都是六块腹肌饭撒的克己复礼看到撒酒疯黎噶搜ID股份分开那份礼物来自空间的佛i阿哥辣椒素的弗兰克为列宁格勒';
     Random random = Random();
+    int i = 0;
     imageUrls.forEach((element) {
       int titleStart = random.nextInt(content.length - 5);
       int subTitleStart = random.nextInt(content.length - 5);
       dataList.add(
         GridInfo(
+          index: i,
+          aboveIcon: random.nextInt(3).toString(),
           icon: random.nextInt(9).toString(),
           subTitle: content.substring(titleStart, titleStart + random.nextInt(4) + 1),
           title: content.substring(subTitleStart, subTitleStart + random.nextInt(4) + 1),
@@ -91,7 +124,127 @@ class _DelayBuildWidgetTestPageState extends State<DelayBuildWidgetTestPage> {
               'https://images.pexels.com/photos/$element/pexels-photo-$element.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
         ),
       );
+      i++;
     });
+  }
+
+  Widget pageBuild({ScrollController controller, Widget childBuilder(GridInfo info)}) {
+    return GridView.builder(
+      controller: controller,
+      itemCount: dataList.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 0,
+        mainAxisSpacing: 0,
+        childAspectRatio: 0.8,
+      ),
+      itemBuilder: (_, index) {
+        GridInfo info = dataList[index];
+        return Padding(
+          padding: const EdgeInsets.all(4),
+          child: Column(
+            children: [
+              Expanded(
+                child: LayoutBuilder(builder: (_, constraints) {
+                  return SizedBox(
+                    width: constraints.maxWidth,
+                    height: constraints.maxHeight,
+                    child: childBuilder(info),
+                  );
+                }),
+              ),
+              Row(
+                children: [
+                  Text(
+                    info.title,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.red,
+                    ),
+                  ),
+                  Image.asset(
+                    ImageHelper.image('icon_${info.icon}.png'),
+                    width: 30,
+                  ),
+                  Spacer(),
+                  Text(
+                    info.subTitle,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget item(GridInfo info) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(8),
+      child: SizedBox(
+        width: BoxConstraints.expand().maxWidth,
+        child: Stack(
+          children: [
+            // Image.network(
+            //   info.url,
+            //   fit: BoxFit.contain,
+            // ),
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.red[100],
+                        borderRadius: BorderRadius.circular(4),
+                        boxShadow: [BoxShadow(color: ColorHelper.DividerColor, spreadRadius: 1, blurRadius: 4)],
+                      ),
+                      child: Text(
+                        info.title + info.subTitle,
+                        style: TextStyle(
+                          fontSize: 13,
+                          color: ColorHelper.Black153,
+                        ),
+                      ),
+                    ),
+                    CircleAvatar(
+                      backgroundColor: Colors.yellow,
+                      child: Text(
+                        info.index.toString(),
+                      ),
+                    ),
+                  ],
+                ),
+                Image.asset(
+                  ImageHelper.image(
+                    'icon_a_${info.aboveIcon}.png',
+                  ),
+                  width: 40,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      info.title,
+                      style: TextStyle(fontSize: 16, color: Colors.green),
+                    ),
+                    Text(
+                      info.subTitle,
+                      style: TextStyle(fontSize: 14, color: Colors.black12),
+                    ),
+                  ],
+                ),
+                Text(
+                  info.subTitle + info.title,
+                  style: TextStyle(fontSize: 18, color: Colors.yellow[100]),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   @override
@@ -101,90 +254,173 @@ class _DelayBuildWidgetTestPageState extends State<DelayBuildWidgetTestPage> {
         title: Text('延时构建小部件测试'),
         actions: [
           FlatButton(
-            onPressed: () {},
+            onPressed: () {
+              ScrollController scrollController =
+                  _tabController.index == 0 ? _firstScrollController : _secondScrollController;
+              if (scrollController.offset > 100) {
+                scrollController.animateTo(0, duration: Duration(milliseconds: 500), curve: Curves.linear);
+              } else {
+                scrollController.animateTo(7000, duration: Duration(milliseconds: 500), curve: Curves.linear);
+              }
+            },
             child: Text('刷新'),
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: GridView.builder(
-          itemCount: dataList.length,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 0,
-            mainAxisSpacing: 0,
-            childAspectRatio: 0.8,
+      body: Column(
+        children: [
+          Container(
+              height: 44,
+              child: TabBar(
+                controller: _tabController,
+                labelColor: Colors.red,
+                onTap: (index) {
+                  _pageController.animateToPage(index, duration: Duration(milliseconds: 100), curve: Curves.linear);
+                },
+                tabs: [Text('延时构建'), Text('不延时')],
+              )),
+          Expanded(
+            child: PageView(
+              controller: _pageController,
+              physics: NeverScrollableScrollPhysics(),
+              children: [
+                pageBuild(
+                  controller: _firstScrollController,
+                  childBuilder: (info) {
+                    return DelayBuildChild(
+                      info: info,
+                      child: item(info),
+                    );
+                  },
+                ),
+                pageBuild(
+                  controller: _secondScrollController,
+                  childBuilder: (info) {
+                    return item(info);
+                  },
+                ),
+              ],
+            ),
           ),
-          itemBuilder: (_, index) {
-            GridInfo info = dataList[index];
-            return Padding(
-              padding: const EdgeInsets.all(4),
-              child: Column(
-                children: [
-                  Expanded(
-                    child: 
-                    // SizedBox(),
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(8),
-                      child: Stack(
-                        children: [
-                          Image.network(
-                            info.url,
-                            fit: BoxFit.contain,
-                          ),
-                          Text(
-                            info.title + info.subTitle,
-                            style: TextStyle(
-                              fontSize: 13,
-                              color: ColorHelper.Black153,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        info.title,
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.red,
-                        ),
-                      ),
-                      Image.asset(ImageHelper.image('icon_${info.icon}.png'),width: 30,),
-                      Spacer(),
-                      Text(
-                        info.subTitle,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            );
-          },
-        ),
+        ],
       ),
     );
   }
 }
 
 class DelayBuildChild extends StatefulWidget {
+  final GridInfo info;
   final Widget child;
   final Widget placeholder;
-  const DelayBuildChild({Key key, this.child, this.placeholder}) : super(key: key);
+  const DelayBuildChild({Key key, this.child, this.placeholder, this.info}) : super(key: key);
   @override
   _DelayBuildChildState createState() => _DelayBuildChildState();
+
+  @override
+  DelayBuildElement createElement() {
+    return DelayBuildElement(this);
+  }
+}
+
+class DelayBuildElement extends StatefulElement {
+  DelayBuildElement(DelayBuildChild widget) : super(widget);
+
+  @override
+  void update(covariant DelayBuildChild newWidget) {
+    (state as _DelayBuildChildState).info.valid = false;
+    super.update(newWidget);
+  }
 }
 
 class _DelayBuildChildState extends State<DelayBuildChild> {
-  RenderObject a;
+  bool canBuild = false;
+  BuildInfo info;
+
+  @override
+  void dispose() {
+    super.dispose();
+    info.valid = false;
+    print('dispose:${widget.info.index}');
+  }
+
+  @override
+  void deactivate() {
+    info.valid = false;
+    super.deactivate();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState:${widget.info.index}');
+    print('list length:${DelayBuildManager._list.length}');
+    info = BuildInfo(
+      rebuild: () {
+        setState(() {
+          canBuild = true;
+        });
+      },
+    );
+    delayBuildManager.add(info);
+  }
+
+  @override
+  void didUpdateWidget(covariant DelayBuildChild oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    info.valid = false;
+    canBuild = false;
+    info = BuildInfo(
+      rebuild: () {
+        setState(() {
+          canBuild = true;
+        });
+      },
+    );
+    delayBuildManager.add(info);
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('build:${widget.info.index},canBuild:$canBuild');
     return RepaintBoundary(
-      child: widget.child,
+      child: !canBuild ? SizedBox() : widget.child,
     );
   }
+}
+
+DelayBuildManager delayBuildManager = DelayBuildManager();
+
+class DelayBuildManager {
+  static ListQueue<BuildInfo> _list = ListQueue<BuildInfo>();
+  bool get haveRebuildAction => true;
+  bool isRunning = false;
+  void add(BuildInfo info) {
+    _list.add(info);
+    if (!isRunning) {
+      actionNext();
+    }
+  }
+
+  void actionNext() {
+    if (_list.isNotEmpty) {
+      isRunning = true;
+      BuildInfo info = _list.last;
+      if (info != null && info.valid) {
+        info.rebuild?.call();
+      }
+      _list.removeLast();
+      Future.delayed(Duration(milliseconds: 64), () {
+        actionNext();
+      });
+    } else {
+      isRunning = false;
+    }
+  }
+}
+
+class BuildInfo {
+  bool valid;
+  final VoidCallback rebuild;
+
+  BuildInfo({this.rebuild, this.valid = true});
 }
