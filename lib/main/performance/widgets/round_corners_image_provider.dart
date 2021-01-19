fe0391f72f48d5c79e12b2ff64c3446016d69610
liboxiang <1169778535@qq.com>
2021-01-20 00:12:06 +0800

    【修改】+ 图片圆角：完善图片圆角生成

--- lib/main/performance/widgets/round_corners_image_provider.dart
+++ lib/main/performance/widgets/round_corners_image_provider.dart
@@ -2,11 +2,10 @@ import 'dart:async';
 import 'dart:io';
 import 'dart:math';
 import 'dart:typed_data';
-import 'dart:ui';
+import 'dart:ui' as ui;
 
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:cached_network_image/src/image_provider/_image_provider_io.dart'
-    if (dart.library.html) 'package:cached_network_image/src/image_provider/_image_provider_web.dart'
+import 'package:cached_network_image/src/image_provider/_image_provider_io.dart' if (dart.library.html) 'package:cached_network_image/src/image_provider/_image_provider_web.dart'
     as CachedNetworkImage;
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:image/image.dart' as IMG;
@@ -14,13 +13,11 @@ import 'package:flutter_summary/main/performance/widgets/round_corners_image.dar
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 // ignore: implementation_imports
-import 'package:flutter/src/painting/_network_image_io.dart'
-    if (dart.library.html) 'package:flutter/src/painting/_network_image_web.dart' as network_image;
+import 'package:flutter/src/painting/_network_image_io.dart' if (dart.library.html) 'package:flutter/src/painting/_network_image_web.dart' as network_image;
 
 import 'isolate_manager.dart';
 
-final IsolateManager _isolateManager =
-    IsolateManager(isolateFunction: _createRoundCornerIsolateMethod, reverseOrder: true, keepRunning: true);
+final IsolateManager _isolateManager = IsolateManager(isolateFunction: _createRoundCornerIsolateMethod, reverseOrder: true, keepRunning: true);
 
 enum ClipLocation {
   Start,
@@ -51,9 +48,7 @@ mixin CornerAndClipKeyMixin on AssetBundleImageKey {
         other.cornerRadius == cornerRadius &&
         (cornerRadius == null || other.cornerColor == cornerColor) &&
         other.haveValidShowSize == haveValidShowSize &&
-        (haveValidShowSize
-            ? (other.showHeight == showHeight && other.showWidth == showWidth && other.clipLocation == clipLocation)
-            : true);
+        (haveValidShowSize ? (other.showHeight == showHeight && other.showWidth == showWidth && other.clipLocation == clipLocation) : true);
   }
 
   @override
@@ -67,48 +62,70 @@ mixin CornerAndClipProviderMixin<T> on ImageProvider<T> {
   //cornerRadius圆角外围部分的颜色
   Color get cornerColor;
   //图片显示的大小，如果设置了图片显示宽高，会按图片显示宽高比截取原图
-  double get showWidth;
-  double get showHeight;
+  int get cacheImageWidth;
+  int get cacheImageHeight;
 
   //showWidth、showHeight设置后裁取的位置
   //如果设置ClipLocation.Start，则当原始图片过长的时候从头部截取宽高比为clipRatio的图片。
   ClipLocation get clipLocation;
 
+  Size _imageShowSize;
+
+  @override
+  Future<T> obtainKey(ImageConfiguration configuration) {
+    _imageShowSize = configuration.size;
+    return super.obtainKey(configuration);
+  }
+
   @override
   ImageStreamCompleter load(key, DecoderCallback decode) {
-    final DecoderCallback decodeRoundCorners =
-        (Uint8List bytes, {int cacheWidth, int cacheHeight, bool allowUpscaling}) async {
+    final DecoderCallback decodeRoundCorners = (Uint8List bytes, {int cacheWidth, int cacheHeight, bool allowUpscaling}) async {
       assert(() {
         print('CornerAndClipProviderMixin load');
         return true;
       }());
+
       Uint8List uint8List;
-      if (cornerRadius != null ||
-          (showWidth != null &&
-              showHeight != null &&
-              showHeight > 0 &&
-              showWidth > 0 &&
-              showHeight != double.nan &&
-              showHeight != double.infinity &&
-              showWidth != double.nan &&
-              showWidth != double.infinity)) {
-        var tmpUint8List = await _isolateManager.send(
-          _IsolateMessage(
-            bytes: bytes,
-            cornerRadius: cornerRadius,
-            color: cornerColor,
-            showHeight: showHeight,
-            showWidth: showWidth,
-            clipLocation: clipLocation,
-          ),
+      if (cacheImageWidth != null || cacheImageHeight != null) {
+        uint8List = await decode(bytes, cacheWidth: cacheImageWidth, cacheHeight: cacheImageHeight, allowUpscaling: false).then((value) async {
+          var image = (await value.getNextFrame().catchError((onError) => null))?.image;
+          if (image is ui.Image) {
+            return (await image.toByteData().catchError((onError) => null))?.buffer?.asUint8List();
+          }
+          return null;
+        }).catchError(
+          (onError) => null,
         );
+      }
+      bool isRawData = uint8List != null;
+      uint8List ??= bytes;
+      // print('start ${DateTime.now().toIso8601String()}');
+      // print('decode end ${DateTime.now().toIso8601String()}===${uint8List.length}');
+
+      // Uint8List uint8List;
+      if (cornerRadius != null && cornerRadius > 1) {
+        var tmpUint8List = await _isolateManager
+            .send(
+              _IsolateMessage(
+                bytes: uint8List,
+                isRawData: isRawData,
+                cornerRadius: cornerRadius,
+                color: cornerColor,
+                showSize: _imageShowSize,
+                clipLocation: clipLocation,
+              ),
+            )
+            .catchError((onError) => null);
         if (tmpUint8List is Uint8List) {
+          isRawData = true;
           uint8List = tmpUint8List;
         }
       }
-      uint8List ??= bytes;
-      return decode(uint8List,
-          cacheWidth: cacheWidth, cacheHeight: cacheHeight, allowUpscaling: allowUpscaling);
+      if (isRawData) {
+        return _decodeImageFromPixels(uint8List, 200, 250, ui.PixelFormat.rgba8888);
+      } else {
+        return decode(bytes, cacheWidth: cacheWidth, cacheHeight: cacheHeight, allowUpscaling: allowUpscaling ?? false);
+      }
     };
     return super.load(key, decodeRoundCorners);
   }
@@ -122,9 +139,7 @@ mixin CornerAndClipProviderMixin<T> on ImageProvider<T> {
         other.cornerRadius == cornerRadius &&
         (cornerRadius == null || other.cornerColor == cornerColor) &&
         other.haveValidShowSize == haveValidShowSize &&
-        (haveValidShowSize
-            ? (other.showHeight == showHeight && other.showWidth == showWidth && other.clipLocation == clipLocation)
-            : true);
+        (haveValidShowSize ? (other.showHeight == showHeight && other.showWidth == showWidth && other.clipLocation == clipLocation) : true);
   }
 
   @override
@@ -358,8 +373,7 @@ class RoundCornersNetworkImage extends network_image.NetworkImage with CornerAnd
   }
 }
 
-class RoundCornerCachedNetworkImage extends CachedNetworkImage.CachedNetworkImageProvider
-    with CornerAndClipProviderMixin {
+class RoundCornerCachedNetworkImage extends CachedNetworkImage.CachedNetworkImageProvider with CornerAndClipProviderMixin {
   //图片显示的大小，如果设置了图片显示宽高，会按图片显示宽高比截取原图
   final double showWidth;
   final double showHeight;
@@ -394,19 +408,10 @@ class RoundCornerCachedNetworkImage extends CachedNetworkImage.CachedNetworkImag
 }
 
 Future _createRoundCornerIsolateMethod(dynamic info) async {
-  if (info is _IsolateMessage &&
-      ((info.showHeight != null && info.showHeight > 0 && info.showWidth != null && info.showWidth > 0) ||
-          info.cornerRadius != null)) {
+  if (info is _IsolateMessage) {
     IMG.Image imageInfo = IMG.decodeImage(info.bytes);
     double scale;
-    if (info.showHeight != null &&
-        info.showHeight > 0 &&
-        info.showWidth != null &&
-        info.showWidth > 0 &&
-        info.showHeight != double.nan &&
-        info.showHeight != double.infinity &&
-        info.showWidth != double.nan &&
-        info.showWidth != double.infinity) {
+    if (info.showSize != null && info.showSize.width != null && info.showSize.height != null){
       scale = min(imageInfo.height / info.showHeight, imageInfo.width / info.showWidth);
       int targetHeight = (info.showHeight * scale).toInt();
       int targetWidth = (info.showWidth * scale).toInt();
@@ -456,24 +461,36 @@ Future _createRoundCornerIsolateMethod(dynamic info) async {
   return null;
 }
 
+Future<Codec> _decodeImageFromPixels(Uint8List pixels, int width, int height, PixelFormat format) {
+  ImmutableBuffer.fromUint8List(pixels).then((ImmutableBuffer buffer) {
+    final ImageDescriptor descriptor = ImageDescriptor.raw(
+      buffer,
+      width: width,
+      height: height,
+      pixelFormat: format,
+    );
+
+    return descriptor.instantiateCodec();
+  });
+}
+
 class _IsolateMessage {
   final Uint8List bytes;
+  final bool isRawData;
   final int cornerRadius;
   final Color color;
 
-  //图片显示的大小，如果设置了图片显示宽高，会按图片显示宽高比截取原图
-  final double showWidth;
-  final double showHeight;
   //showSize设置后裁取的位置
   //如果设置ClipLocation.Start，则当原始图片过长的时候从头部截取宽高比为clipRatio的图片。
   final ClipLocation clipLocation;
+  final Size showSize;
 
   _IsolateMessage({
     @required this.bytes,
+    @required this.isRawData,
     this.cornerRadius,
     this.color,
-    this.showHeight,
-    this.showWidth,
+    this.showSize,
     this.clipLocation,
-  });
+  }) : assert(bytes != null && isRawData != null);
 }
